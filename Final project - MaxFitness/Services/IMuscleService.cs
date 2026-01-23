41b2cf3c802da5a66bfd8c7a7e414861421c05f6
Maxi <kaloyan-maksimilian.mitov.highschool@buditel.bg>
2026-01-23 11:15:32 +0200

    Muscle service

---
+++ Final project - MaxFitness/Services/IMuscleService.cs
@@ -0,0 +1,7 @@
+﻿using Final_project___MaxFitness.Models;
+
+public interface IMuscleService
+{
+    Task<MuscleProgress> GetMuscleStatsAsync(string muscleName);
+    Task<List<ExerciseDetail>> GetExercisesAsync(string muscleName);
+}
---
+++ Final project - MaxFitness/Services/MuscleService.cs
@@ -0,0 +1,6 @@
+﻿namespace Final_project___MaxFitness.Services
+{
+    public class MuscleService
+    {
+    }
+}
