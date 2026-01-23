6942c7ee4d5918f0d3f0da0ad77f388bea2c793c
Maxi <kaloyan-maksimilian.mitov.highschool@buditel.bg>
2026-01-23 11:11:30 +0200

    models

---
+++ Final project - MaxFitness/Models/ExerciseDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_project___MaxFitness.Models
+{
+    public class ExerciseDetail
+    {
+        public string Name { get; set; } = string.Empty;
+        public string TargetReps { get; set; } = "3 x 12";
+        public string Intensity { get; set; } = "75% 1RM";
+        public string Difficulty { get; set; } = "Intermediate";
+    }
+}
---
+++ Final project - MaxFitness/Models/MuscleProgress.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_project___MaxFitness.Models
+{
+    public class MuscleProgress
+    {
+        public string Name { get; set; } = string.Empty;
+        public int ProgressPercentage { get; set; }
+        public string CurrentPR { get; set; } = "0 lbs";
+        public string Icon { get; set; } = "⚡";
+    }
+}
