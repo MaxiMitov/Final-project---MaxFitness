444e264bf79e26fac2eca793da42b19b1e56fe0b
Maxi <kaloyan-maksimilian.mitov.highschool@buditel.bg>
2026-02-19 19:56:11 +0200

    added GetExercises API endpoint to fetch from database

--- Final project - MaxFitness/Controllers/WorkoutController.cs
+++ Final project - MaxFitness/Controllers/WorkoutController.cs
@@ -1,12 +1,32 @@
+using Final_project___MaxFitness.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Final_project___MaxFitness.Controllers
 {
     public class WorkoutController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public WorkoutController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetExercises(string muscleGroup)
+        {
+            var exercises = await _context.Exercises
+                .Where(e => e.MuscleGroup == muscleGroup.ToLower())
+                .Select(e => new { e.Name, e.Type })
+                .ToListAsync();
+
+            return Json(exercises);
         }
     }
 }
