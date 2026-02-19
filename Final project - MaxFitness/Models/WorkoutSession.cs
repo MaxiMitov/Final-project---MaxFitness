f8c2bd1c5bcf81756faacf51a39fdbd1798de038
Maxi <kaloyan-maksimilian.mitov.highschool@buditel.bg>
2026-02-19 20:22:11 +0200

    added WorkoutSession model

---
+++ Final project - MaxFitness/Models/WorkoutSession.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Final_project___MaxFitness.Models
+{
+    public class WorkoutSession
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CompletedAt { get; set; } = DateTime.UtcNow;
+
+        public int DurationSeconds { get; set; }
+
+        public int IntensityScore { get; set; }
+
+        public int CaloriesBurned { get; set; }
+
+        public double TotalVolume { get; set; }
+
+        public int TotalSets { get; set; }
+
+        public int TotalReps { get; set; }
+
+        public List<WorkoutExerciseLog> ExerciseLogs { get; set; } = new();
+    }
+}
