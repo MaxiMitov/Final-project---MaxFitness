b5d0c044ae37d038af427fec8cf3c9c2fe042a8c
Maxi <kaloyan-maksimilian.mitov.highschool@buditel.bg>
2026-03-22 21:08:01 +0200

    config: setup identity services and authentication middleware in Program.cs

--- Final project - MaxFitness/Program.cs
+++ Final project - MaxFitness/Program.cs
@@ -13,12 +13,21 @@ namespace Final_project___MaxFitness
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
 
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddEntityFrameworkStores<AppDbContext>();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>();
 
             builder.Services.AddScoped<IMuscleService, MuscleService>();
             builder.Services.AddScoped<IWorkoutStatsService, WorkoutStatsService>();
