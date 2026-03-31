2b0e0aa5242f0af7fb6260d7360ae6f8d7e272fc
Maxi <kaloyan-maksimilian.mitov.highschool@buditel.bg>
2026-03-31 19:49:51 +0300

    added admin role and seeded admin user

--- Final project - MaxFitness/Program.cs
+++ Final project - MaxFitness/Program.cs
@@ -24,12 +24,37 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options =>
     options.Password.RequireUppercase = false;
     options.Password.RequireLowercase = false;
 })
+.AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddScoped<IMuscleService, MuscleService>();
 builder.Services.AddScoped<IWorkoutStatsService, WorkoutStatsService>();
 
 var app = builder.Build();
+
+// Seed admin role and user
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    if (!await roleManager.RoleExistsAsync("Admin"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+
+    var adminUser = await userManager.FindByNameAsync("admin");
+    if (adminUser == null)
+    {
+        adminUser = new IdentityUser { UserName = "admin", Email = "admin@maxfitness.com", EmailConfirmed = true };
+        await userManager.CreateAsync(adminUser, "admin123");
+        await userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+    else if (!await userManager.IsInRoleAsync(adminUser, "Admin"))
+    {
+        await userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+}
 
 if (!app.Environment.IsDevelopment())
 {
