cbcd02aa5978235d33567f712700f0a9db3ae5b9
Maxi <kaloyan-maksimilian.mitov.highschool@buditel.bg>
2026-03-31 18:15:12 +0300

    added personal records and weekly volume models

--- Final project - MaxFitness/Models/MuscleProgress.cs
+++ Final project - MaxFitness/Models/MuscleProgress.cs
@@ -6,5 +6,28 @@
         public int ProgressPercentage { get; set; }
         public string CurrentPR { get; set; } = "0 lbs";
         public string Icon { get; set; } = "⚡";
+        public int WeeklySets { get; set; }
+        public string RecoveryStatus { get; set; } = "Unknown";
+        public string NextSession { get; set; } = "N/A";
+        public int TotalExercises { get; set; }
+        public int CompoundCount { get; set; }
+        public int IsolationCount { get; set; }
+        public int BodyweightCount { get; set; }
+        public List<PersonalRecord> PersonalRecords { get; set; } = new();
+        public List<WeeklyVolume> MonthlyProgress { get; set; } = new();
+        public string TrendPercent { get; set; } = "0";
+    }
+
+    public class PersonalRecord
+    {
+        public string Weight { get; set; } = "";
+        public string Date { get; set; } = "";
+    }
+
+    public class WeeklyVolume
+    {
+        public string Label { get; set; } = "";
+        public int Sets { get; set; }
+        public int HeightPercent { get; set; }
     }
 }
